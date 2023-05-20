5142c0f0545b13b659bf9fa7e7d5d917bb3b1e3a
Albert Portnoy <albert@albertp.dev>
2023-05-19 22:45:54 -0500

    v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { Logger } from "replugged";
 const logger = Logger.plugin("StoreUpdaterDemo");
 
 export function start(): void {
-  logger.log("v1.0.1 of StoreUpdaterDemo (Store source)");
+  logger.log("v1.0.2 of StoreUpdaterDemo (Store source)");
 }
