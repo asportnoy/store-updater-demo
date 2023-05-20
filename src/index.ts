f075aae55da3c1af096bef1d76bfeabec006a017
Albert Portnoy <albert@albertp.dev>
2023-05-19 22:39:39 -0500

    v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Logger } from "replugged";
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const logger = Logger.plugin("StoreUpdaterDemo");
 
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
-}
-
-export function stop(): void {
-  inject.uninjectAll();
+export function start(): void {
+  logger.log("v1.0.0 of StoreUpdaterDemo (GitHub source)");
 }
